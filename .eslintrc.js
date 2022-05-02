65d6c654df714ccbb85c6072d065662520fe04bd
Hai Nguyen <hai.nguyen@kyanon.digital>
2022-05-02 14:14:10 +0700

    Update follow eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,49 @@
 module.exports = {
-  'env': {
-      'commonjs': true,
-      'es2021': true,
-      'node': true
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest/globals": true
   },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-      'ecmaVersion': 'latest'
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
   },
-  'rules': {
-      'indent': [
-          'error',
-          4
+  "plugins": [
+    "react", "jest"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+        2
       ],
-      'linebreak-style': [
-          'error',
-          'unix'
-      ],
-      'quotes': [
-          'error',
-          'single'
-      ],
-      'semi': [
-          'error',
-          'never'
-      ]
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": "error",
+    "react/prop-types": 0
   }
 }
